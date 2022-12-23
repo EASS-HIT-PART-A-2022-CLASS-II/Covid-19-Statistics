79055884453cfda0b97c753582a69274a8ce4e25
SaharRaz <smartcity.db.hit@gmail.com>
2022-12-23 18:31:55 +0200

    update

--- src/Backend/BackEnd.py
+++ src/Backend/BackEnd.py
@@ -51,15 +51,25 @@ def specicif_country(country):
     for i in response_data:
         singleCountry.Country = "" # solve the saving last input problem
         singleCountry.ThreeLetterSymbol = ""
-        singleCountry.TotalCases = 0
         singleCountry.Infection_Risk = 0
-        singleCountry.Population = 0
+        singleCountry.TotalCases = 0
+        singleCountry.TotalDeaths = 0
+        singleCountry.TotalRecovered = ""
+        singleCountry.TotalTests = ""
+        singleCountry.Population = ""
+        singleCountry.ActiveCases = 0
+        singleCountry.SeriousCritical = 0
         if i['Country'] == country:  # search by country - user input
             singleCountry.Country=i['Country'] # value i want to show
             singleCountry.ThreeLetterSymbol=i['ThreeLetterSymbol'] # value i want to show
-            singleCountry.TotalCases=i['TotalCases'] # value i want to show
             singleCountry.Infection_Risk=i['Infection_Risk'] # value i want to show
+            singleCountry.TotalCases=i['TotalCases'] # value i want to show
+            singleCountry.TotalDeaths=i['TotalDeaths'] # value i want to show
+            singleCountry.TotalRecovered=i['TotalRecovered'] # value i want to show
+            singleCountry.TotalTests=i['TotalTests'] # value i want to show
             singleCountry.Population=i['Population'] # value i want to show
+            singleCountry.ActiveCases=i['ActiveCases'] # value i want to show
+            singleCountry.SeriousCritical=i['SeriousCritical'] # value i want to show
             singleCountry.Message="OK"
             return singleCountry
         
