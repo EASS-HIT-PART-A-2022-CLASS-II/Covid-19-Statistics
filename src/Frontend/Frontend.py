c09e220b6511b9f86b9d820e23349f9362084246
SaharRaz <smartcity.db.hit@gmail.com>
2022-12-27 13:50:36 +0200

    update

--- src/Frontend/Frontend.py
+++ src/Frontend/Frontend.py
@@ -1,86 +1,66 @@
 import streamlit as st
 import requests
-import matplotlib.pyplot as plt
-import json
-import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
-
-# Set the base URL of the backend API
-BASE_URL = "http://localhost:8000"
-
-# Set the API key and host for the COVID API
-API_KEY = "a463c18a92msha3eb294601ac463p198518jsna8e685091d4c"
-API_HOST = "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
-
-def get_world_data():
-    # Call the get_all_world_covid_data endpoint to retrieve the world data
-    response = requests.get(f"{BASE_URL}/get_all_world_covid_data", headers={
-        "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": API_HOST
-    })
-    return response.json()
-
-def get_countries_data():
-    # Call the get_all_countries_covid_data endpoint to retrieve the data for all countries
-    response = requests.get(f"{BASE_URL}/get_all_countries_covid_data", headers={
-        "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": API_HOST
-    })
-    return response.json()
-
-def get_country_data(country):
-    # Call the get_specific_country_covid_data endpoint to retrieve the data for a specific country
-    response = requests.get(f"{BASE_URL}/get_specific_country_covid_data/{country}", headers={
-        "X-RapidAPI-Key": API_KEY,
-        "X-RapidAPI-Host": API_HOST
-    })
-    return response.json()
-
-# Create a side menu with options to view the world data or view data for a specific country
 st.sidebar.title("Options")
-view_world_data = st.sidebar.button("View world data")
-view_country_data = st.sidebar.button("View data for a specific country")
-view_pie_chart = st.sidebar.button("5M and above")
+option = st.sidebar.selectbox("Select an option", ["Single Country View", "World Wide", "All Countries Charts"])
 
-# If the user wants to view the world data, call the get_world_data function and display the data
-if view_world_data:
-    world_data = get_world_data()
-    st.title("World data")
-    st.json(world_data)
-    
-    countries = get_countries_data()
-    labels = [country['Country'] for country in countries]
-    cases = [country['TotalCases'] for country in countries]
-    plt.pie(cases, labels=labels)
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.pyplot() 
-    
-# If the user wants to view data for a specific country, display a dropdown menu to select the country
+if option == "Single Country View":
+  # Call the /get_specific_country_covid_data endpoint and pass the user-specified country name as a parameter
+  country = st.sidebar.text_input("Enter a country name")
+  response = requests.get(f"http://localhost:8000/get_specific_country_covid_data/{country}")
+  data = response.json()
 
-if view_country_data:
-    st.title("Country data")
-    countries = get_countries_data()
-    country_names = [Country['Country'] for Country in countries]
-    selected_country = st.selectbox("Select a country", country_names)
-    country_data = get_country_data(selected_country)
+elif option == "World Wide":
+  # Call the /get_all_world_covid_data endpoint
+  response = requests.get("http://localhost:8000/get_all_world_covid_data")
+  data = response.json()
 
-    # Convert the country data dictionary into a Pandas DataFrame
-    df = pd.DataFrame.from_dict(country_data, orient='index', columns=['Value'])
+elif option == "All Countries Charts":
+  # Call the /get_all_countries_covid_data endpoint
+  response = requests.get("http://localhost:8000/get_all_countries_covid_data")
+  data = response.json()
 
-    # Display the table using st.table
-    st.table(df)
+if option == "Single Country View":
+  st.write(f"Country: {data['Country']}")
+  st.write(f"Three Letter Symbol: {data['ThreeLetterSymbol']}")
+  st.write(f"Infection Risk: {data['Infection_Risk']}")
+  st.write(f"Total Cases: {data['TotalCases']}")
+  st.write(f"Total Deaths: {data['TotalDeaths']}")
+  st.write(f"Total Recovered: {data['TotalRecovered']}")
+  st.write(f"Total Tests: {data['TotalTests']}")
+  st.write(f"Population: {data['Population']}")
+    # Create the chart
+  countries = [data['Country']]
+  total_cases = [data['TotalCases']]
+  total_deaths = [data['TotalDeaths']]
+
+  fig = make_subplots(rows=1, cols=2)
+
+  fig.add_trace(go.Bar(x=countries, y=total_cases, name="Total Cases"), row=1, col=1)
+  fig.add_trace(go.Bar(x=countries, y=total_deaths, name="Total Deaths"), row=1, col=2)
+
+  fig.update_layout(barmode='group')
+  st.plotly_chart(fig)
 
 
+if option == "World Wide" or option == "All Countries Charts":
+  countries = [d['Country'] for d in data]
+  total_cases = [d['TotalCases'] for d in data]
+  total_deaths = [d['TotalDeaths'] for d in data]
+  total_recovered = [d['TotalRecovered'] for d in data]
 
-if view_pie_chart:
-    st.title("Pie chart of different countries with 5M cases and above!")
-    countries = get_countries_data()
-    # Filter the countries list to only include countries with more than 5,000,000 cases
-    countries_filtered = [country for country in countries if country['TotalCases'] > 5000000]
-    labels = [country['Country'] for country in countries_filtered]
-    cases = [country['TotalCases'] for country in countries_filtered]
-    plt.pie(cases, labels=labels)
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    st.pyplot() 
-
-   
+  fig = make_subplots(rows=1, cols=3)
+  fig.add_trace(go.Bar(x=countries, y=total_cases, name="Total Cases"), row=1, col=1)
+  fig.add_trace(go.Bar(x=countries, y=total_deaths, name="Total Deaths"), row=1, col=2)
+  fig.add_trace(go.Bar(x=countries, y=total_recovered, name="Total Recovered"), row=1, col=3)
+  fig.update_layout(barmode='group')
+  
+  # Create the table data
+  table_data = [{'Country': country, 'Total Cases': total_cases, 'Total Deaths': total_deaths, 'Total Recovered': total_recovered} for country, total_cases, total_deaths, total_recovered in zip(countries, total_cases, total_deaths, total_recovered)]
+  
+  
+  st.plotly_chart(fig)
+# Display the table
+  st.table(table_data)
